4f9f9e31b426b98f479cb65f3b9330a5db1a8a0d
Jenimpatel <34350472+Jenimpatel@users.noreply.github.com>
2018-08-13 14:53:39 +0530

    Update Obj_det.py

--- Object_detection/Obj_det.py
+++ Object_detection/Obj_det.py
@@ -9,7 +9,7 @@ FLANN_INDEX_KDITREE=0
 flannParam=dict(algorithm=FLANN_INDEX_KDITREE,tree=5)
 flann=cv2.FlannBasedMatcher(flannParam,{})
 
-trainImg=cv2.imread("training/mobile3.jpg",0)
+trainImg=cv2.imread("mobile3.jpg",0)
 trainKP,trainDesc=detector.detectAndCompute(trainImg,None)
 
 cam=cv2.VideoCapture(0)
